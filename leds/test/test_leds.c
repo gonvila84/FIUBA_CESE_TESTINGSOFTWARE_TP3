180d40d18afa090f5fdd4a9f48cdb8f4529489a7
Gonzalo Vila <gonvila84@gmail.com>
2022-07-19 23:42:57 +0200

    Se unifica el formato de los comentarios en el archivo de test

--- leds/test/test_leds.c
+++ leds/test/test_leds.c
@@ -4,35 +4,71 @@
 
 static uint16_t ledsVirtuales;
 
+/**
+ * @brief Se realiza la configuracion inicial de los LED's para todos los tests. Los LED's se inicializan apagados.
+ * 
+ */
+
 void setUp(void) {
     LedsInit(&ledsVirtuales);
 }
+
+
+
+/**
+ * @brief Se realiza el cleanup de cada test tras la ejecucion del test. En este caso no se utiliza.
+ * 
+ */
 
 void tearDown(void) {
 
 }
 
-//Despues de la inicializacion todos los LED's deben quedar apagados.
+
+
+/**
+ * @brief Se verifica que tras la inicializacion todos los LED's esten apagados.
+ * 
+ */
+
 void test_ledsOffAfterCreate(void) {
     uint16_t ledsVirtuales = 0xFFFF;
     LedsInit(&ledsVirtuales);
     TEST_ASSERT_EQUAL_HEX16(0x0000, ledsVirtuales);
 }
 
-//Se puede prender un LED individual.
+
+
+/**
+ * @brief Se verifica que se pueda encender un LED de forma individual.
+ * 
+ */
+
 void test_TurnOnOneLed(void) {
     LedTurnOn(5);
     TEST_ASSERT_EQUAL_HEX16(1 << 4, ledsVirtuales);   
 }
 
-//Se puede apagar un LED individual.
+
+
+/**
+ * @brief Se verifica que se pueda apagar un LED de forma individual.
+ * 
+ */
+
 void test_TurnOffOneLed(void) {
     LedTurnOn(5);
     LedTurnOff(5);
     TEST_ASSERT_EQUAL_HEX16(0x0000, ledsVirtuales);   
 }
 
-//Se pueden prender y apagar multiples LED's.
+
+
+/**
+ * @brief Se verifica que se pueda encender y apagar multiples LED's.
+ * 
+ */
+
 void test_TurnOnAndOffManyLeds(void) {
     LedTurnOn(5);
     LedTurnOn(11);
@@ -41,29 +77,51 @@ void test_TurnOnAndOffManyLeds(void) {
     TEST_ASSERT_EQUAL_HEX16(1 << 4, ledsVirtuales);   
 }
 
-//
+
+
+/**
+ * @brief Se verifica que la funcion retorne un mensaje de error ante un parametro que se encuentra sobre el numero maximo de LED's.
+ * 
+ */
+
 void test_InvalidUpperLimitTurnOnLed(void){
     RegistrarMensaje_Expect(ALERTA, "LedTurnOn", 0, "Numero de led invalido");
     RegistrarMensaje_IgnoreArg_linea();
     LedTurnOn(17);
 }
 
-//Hechos por mi
 
-//Se pueden prender todos los LEDs de una vez.
+
+/**
+ * @brief Se verifica que todos los LED's pueden encenderse de una vez.
+ * 
+ */
+
 void test_TurnOnAllLeds(void){
     LedTurnOnAllAtOnce();
     TEST_ASSERT_EQUAL_HEX16(0xFFFF, ledsVirtuales);
 }
 
-//Se pueden apagar todos los LEDs de una vez.
+
+
+/**
+ * @brief Se verifica que todos los LED's pueden apagarse de una vez.
+ * 
+ */
+
 void test_TurnOffAllLeds(void){
     LedTurnOnAllAtOnce();
     LedTurnOffAllAtOnce();
     TEST_ASSERT_EQUAL_HEX16(0x0, ledsVirtuales);
 }
 
-//Se puede consultar el estado de un led.
+
+
+/**
+ * @brief Se verifica que se pueda consultar el estado de un LED apagado.
+ * 
+ */
+
 void test_queryLedStateOff(){
     bool_t ledState = true;
     ledsVirtuales = 0 << 4;
@@ -71,7 +129,13 @@ void test_queryLedStateOff(){
     TEST_ASSERT_FALSE(ledState);
 }
 
-//Se puede consultar el estado de un led.
+
+
+/**
+ * @brief Se verifica que se pueda consultar el estado de un LED encendido.
+ * 
+ */
+
 void test_queryLedStateOn(){
     bool_t ledState = false;
     ledsVirtuales = 1 << 4;
@@ -79,14 +143,26 @@ void test_queryLedStateOn(){
     TEST_ASSERT_TRUE(ledState);
 }
 
-//Se prueban la funcion de consulta del estado del led on los limites led < 1 y led > 16
+
+
+/**
+ * @brief Se verifica la funcion de consulta del estado del LED on en el limite LED > 16.
+ * 
+ */
+
 void test_queryLedStateUpperLimmit(){
     RegistrarMensaje_Expect(ALERTA, "LedQueryState", 0, "Numero de led invalido");
     RegistrarMensaje_IgnoreArg_linea();
     LedQueryState(17);
 }
 
-//Se prueban la funcion de consulta del estado del led on los limites led < 1 y led > 16
+
+
+/**
+ * @brief Se verifica la funcion de consulta del estado del LED on en el limite LED < 1.
+ * 
+ */
+
 void test_queryLedStateLowerLimmit(){
     RegistrarMensaje_Expect(ALERTA, "LedQueryState", 0, "Numero de led invalido");
     RegistrarMensaje_IgnoreArg_linea();
